c6f895807452413d694de0f7e5d00420efee2509
Saran G <Saran G@saranPc>
2025-11-30 11:54:35 +0530

    chore: initial project setup
    - Added Spring Boot main class
    - Added application.properties
    - Added .gitignore

---
+++ team-vault/team-vault/src/main/java/com/teamvault/TeamVaultApplication.java
@@ -0,0 +1,13 @@
+package com.teamvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamVaultApplication.class, args);
+	}
+
+}
---
+++ team-vault/team-vault/src/test/java/com/teamvault/TeamVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
